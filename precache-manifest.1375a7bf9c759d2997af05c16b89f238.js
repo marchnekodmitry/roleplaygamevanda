9b79c4cfce26d167251270c9e0a725713c40caf3
Dmitriy Marchenko <marchenko.dmitriy.00@gmail.com>
2020-10-12 16:06:02 +0300

    Updates

---
+++ precache-manifest.1375a7bf9c759d2997af05c16b89f238.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b4897c89c913021d4da4b3ca97a3185d",
+    "url": "/roleplaygamevanda/index.html"
+  },
+  {
+    "revision": "3b7586b9200c9ba17345",
+    "url": "/roleplaygamevanda/static/js/2.96ebc064.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/roleplaygamevanda/static/js/2.96ebc064.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "669038a68bd5adac8992",
+    "url": "/roleplaygamevanda/static/js/main.062e020b.chunk.js"
+  },
+  {
+    "revision": "ee950de07a292aee2625",
+    "url": "/roleplaygamevanda/static/js/runtime-main.f1a53e1a.js"
+  }
+]);
